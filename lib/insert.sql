c1da855a89e966e1c5547bf0e36b4bd688acc2e9
yellowpad <diggable.co@gmail.com>
2017-02-13 01:23:09 -0500

    Sql lab

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES ("Mr. Chocolate", "M", 32, "Brown", "Angry", "Dead");
+INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES ("Rowdy", "M", 32, "Brown", "Angry", "Dead");
+INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES ("Tabitha", "M", 32, "Brown", "Angry", "Dead");
+INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES ("Sergeant Brown", "M", 32, "Brown", "Angry", "Dead");
+INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES ("Grinch", "M", 32, "Brown", "Angry", "Dead");
+INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES ("Grinch", "M", 32, "Brown", "Angry", "Dead");
+INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES ("Wendy", "M", 32, "Brown", "Angry", "Dead");
+INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES (NULL, "M", 32, "Brown", "Angry", "Dead");
